923bfc0d16f6612fb022c35e8605f2e18a9f53c9
Max Dahlberg <max.dahlberg1@edu.varberg.se>
2022-10-18 11:01:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labb_2_Arv_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
